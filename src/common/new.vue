c6b3ac7f97f7db42c61d280f71aa1cdd2eb3805f
pf666nb <1340701723@qq.com>
2020-08-27 15:24:44 +0800

    fist comment

---
+++ src/common/new.vue
@@ -0,0 +1,30 @@
+<template>
+<div >   </div>
+</template>
+<script>
+export default {
+name:'',
+filters:{},
+components: {},
+mixins:[],
+     data() {
+         return {}
+     },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {
+},
+  beforeMount() {},
+  mounted() {
+},
+  beforeUpdate() {},
+  updated() {}, 
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+  methods: {}
+  }
+</script>
+<style lang='scss' scoped>
+</style>
